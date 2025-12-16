ecceabffe30d4c2ba51aac4dd5e4ea858cebc67b
Omar Issa <omom2168@gmail.com>
2025-12-16 21:42:43 +0200

    test 2

--- login_handler.php
+++ login_handler.php
@@ -1,1 +1,14 @@
-die('LOGIN HANDLER REACHED');
+<?php
+require_once 'config.php';
+require_once 'User.php';
+
+echo '<pre>';
+
+var_dump($_POST);
+
+$user = new User();
+$result = $user->login($_POST['username'], $_POST['password']);
+
+var_dump($result);
+
+exit;
