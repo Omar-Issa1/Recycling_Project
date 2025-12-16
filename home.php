6088ad07bd7b004200205230a35166fd74336091
Omar Issa <omom2168@gmail.com>
2025-12-16 17:05:24 +0200

    fix path

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-<link rel="stylesheet" href="home.css">
+<link rel="stylesheet" href="/home.css">
     <link rel="website icon" href="photo/Icon.png" type="png">
 <link rel="stylesheet" href="Navbar.css">
 </head>
